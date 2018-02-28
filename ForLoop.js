932ddde64655bf0b1d9bdb55faefe3ca4c751e1f
Everlin Wanjiku <everlinwanjiku@gmail.com>
2018-02-28 16:35:57 +0300

    added a file

--- ForLoop.js
+++ ForLoop.js
@@ -1,4 +1,4 @@
 var DaysOfTheWeek = 7;
-for(i=0; i<=DaysOfTheWeek; i++){
+for(i=0; i<=DaysOfTheWeek; i){
 console.log(i)
 }
---
+++ SwitchCase.js
@@ -0,0 +1,9 @@
+var IsSunday = false
+switch(IsSunday){
+	case true:
+	break;
+	case false:
+	break;
+	default:
+	break;
+}
