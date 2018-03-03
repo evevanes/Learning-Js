4b6d0faf85d1372a6e83ab90249e698fcc9ad6e4
Everlin Wanjiku <everlinwanjiku@gmail.com>
2018-03-03 12:13:57 +0300

    added functions

---
+++ Function.js
@@ -0,0 +1,47 @@
+// This is a void function
+function add(){
+	var number1 = 20;
+	var number2 = 10;
+	var sum = number1 + number2;
+
+	console.log(sum)
+}
+// call the function
+add()
+
+
+function multiply(){
+	var numberOne = 20;
+	var numberTwo = 10;
+	var numberThree = 30;
+	var answer = numberOne * numberTwo * numberThree;
+
+	return answer
+}
+
+var result = multiply()
+console.log(result)
+
+
+function divide(){
+	var numberOne = 20;
+	var numberTwo = 10;
+	var answer = numberOne / numberTwo;
+
+	return answer
+}
+
+var result = divide()
+console.log(result)	
+
+
+function subtract(){
+	var numberOne = 20;
+	var numberTwo = 10;
+	var answer = numberOne - numberTwo;
+
+	return answer
+}
+
+var result = subtract()
+console.log(result)	
